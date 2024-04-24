a225a82fbfcf00cfef67cf2b3faedb59b9731891
zolamendbayar <266408@glenbrook225.org>
2024-04-24 11:55:36 -0500

    apr 24

--- myScripts.js
+++ myScripts.js
@@ -1,19 +1,19 @@
 //CLOCK SCRIPT
 function startTime() {
- const today = new Date();
- let h = today.getHours();
- let m = today.getMinutes();
- let s = today.getSeconds();
- m = checkTime(m);
- s = checkTime(s);
- document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
- setTimeout(startTime, 1000);
-}
-
-function checkTime(i) {
- if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
- return i;
-}
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+   }
+   
+   function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+   }
 //POST-IT NOTE SAVE (JAVASCRIPT)
 if (localStorage["note"]) {
     var note = localStorage["note"];
@@ -38,7 +38,19 @@ if (localStorage["note"]) {
     }
  }
  
- //MUST set “opacity:0” in CSS on #note
+ //CALCULATOR (JAVASCRIPT)
+var showCalc = false;
+function showCalculator(){
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
+}
+
  
 
 
